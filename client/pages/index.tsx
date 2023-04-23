0c01089fd79878581a08eaeaa388a50c96744dbf
makiwebdeveloper <makiwebdeveloper@gmail.com>
2023-04-23 11:50:20 +0200

    client setup

---
+++ client/pages/index.tsx
@@ -0,0 +1,8 @@
+import Home from "@/components/screens/home/page";
+import { NextPage } from "next";
+
+const HomePage: NextPage = () => {
+  return <Home />;
+};
+
+export default HomePage;
---
+++ client/src/components/screens/home/page.tsx
@@ -0,0 +1,11 @@
+import { FC } from "react";
+
+const Home: FC = () => {
+  return (
+    <main>
+      <h1 className="text-3xl">Home Page</h1>
+    </main>
+  );
+};
+
+export default Home;
